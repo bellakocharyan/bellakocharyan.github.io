967fa7ba231921358b213d56b3f1c18f87d03aef
Bella <bellak1985@gmail.com>
2019-08-29 18:26:41 +0400

    4th commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const header = document.getElementById(`header`);
+const header = document.querySelector(`#header`);
 header.style.background = `white`;
 header.style.width = `initial`;
 setTimeout(() => {header.style.background = `black`}, 2000);
 
-const title = document.getElementById(`title`);
+const title = document.querySelector(`#title`);
 title.style.color = `black`;
 title.style.width = `initial`;
 setTimeout(() => {title.style.color = `white`}, 2000);
@@ -15,7 +15,7 @@ function typeText(){
 	title.textContent += text.charAt(i) ;
 	i++;
 }
-	setTimeout(typeText, 100);
+	setTimeout(typeText, 120);
 		
 }
 typeText();
